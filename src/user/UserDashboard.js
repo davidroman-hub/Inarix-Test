389f5d80b043ff65d8d8a8f50f72d7726521e23c
David <jobroman83@gmail.com>
2020-09-22 19:37:57 +0200

    task 3 styling router dashboard

--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -1,90 +1,38 @@
-import React,{useEffect,useState} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
-import {isAuth, getCookie, signout } from '../helpers/Helpers';
-import axios from 'axios';
-
-
+import '../styles/components/_router-dashboard.scss';
+import inarixLogo from '../images/inarix-logo-h@3x.png'
 const UserDashboard = () => {
 
-    const token = getCookie('accessToken');
-    const Id = getCookie('accessToken');
 
-    const [values, setValues] = useState({
-        
-        email: '',
-        buttonText: 'Update'
-    })
-
-    const { email} = values;
-
-
-    const loadProfile = ({history}) => {
-        axios({
-            method: 'GET',
-            url:`${process.env.REACT_APP_API}/users/${isAuth()._id}`,
-            headers: {
-                Authorization:`Bearer ${token}`
-            }
-        })
-        .then(response=>{
-            console.log('profile ', response) //<-- populate state
-            const {email} = response.data
-            setValues({...values, email})
-        })
-        .catch(error => {
-            console.log('Profile error', error.response.data.error)
-            if(error.response.status == 401){
-                signout(() => {
-                    history.push('/'); //<-- for signout automatically
-                })
-            } 
-        })
-    }
-
-    const LoadProfileInfo = () => (
+    const RouterDashboard = () => (
         <>
-            <div className=' card mb-5' id="header-content">
-            <h3 className='card-header'>Info User</h3>
-            <ul className='list-group'>
-                {/* <li className='list-group-item'>
-                    Hello! 
-                </li>
-                <li className='list-group-item'>
-                    E-mail: {isAuth().email}
-                </li>
-                <li className='list-group-item'>
-                    Samples: 
-                </li> */}
-            </ul>
-        </div>
-            
-        <div className='card mb-5'>
-            {/* <h4 className='text-center'>Antes de Ordenar es importante actualizar tu información!!</h4> */}
         
-            {/* <h5 className='text-center'>Necesitamos tu dirección de envío y tu telefono para poder contactarte!</h5> */}
-            <li className='list-group-item'>
-                <Link to='/user/private' >User Dashboard</Link>
-            </li>
-            <li className='list-group-item'>
-                <Link to='/admin/dashboard' >Admin Dashboard</Link>
-            </li>
-        </div>
-    </>
-    )
 
-    useEffect(() => {
-        //loadinit(Id, token)
-        //loadProfile()
-        //loadOrders()
-    },[])
+
+            <div className='__links-dashboard'>
+                <div className='__button-1 __user-dashboard'>
+                    <Link to='/user/private'  style={{textDecoration:'none', color:'#232527'}} >User Dashboard</Link>
+                </div>
+                <div className='__button-1'>
+                    <Link to='/admin/dashboard'  style={{textDecoration:'none', color:'#232527'}} >Admin Dashboard</Link>
+                </div>
+            </div>
+        </>
+    )
 
     return(
         <>
-        <div className='container'>
-            <h1 className='mt-5 mb-5'>Dashboard User and Admin</h1>
-            <h3>Here you will see two differents Dashboard as an User and as an Admin</h3>
-            <h4 className='mb-5'>To see the differents function beetween each other</h4>
-                {LoadProfileInfo()}
+        <div className='__info-routerDashboard'>
+            
+            <h1 className='mt-5 mb-5 text-center'>Welcome to Dashboard User and Admin</h1>
+            <div className='image-logo'>
+                <img src={inarixLogo} alt='inari-logo'/>
+            </div>
+            <h3 className='text-center'>Here you will see two differents Dashboard as an User and as an Admin</h3>
+            <h4 className='mb-5 text-center'>To see the differents function beetween each other</h4>
+                
+                {RouterDashboard()}
 
         </div>
         </>
