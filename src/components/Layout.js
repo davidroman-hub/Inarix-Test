3086ee5bc9a0385b421f40a272f679e68c5cbd39
David <jobroman83@gmail.com>
2020-09-22 15:25:11 +0200

    task 1 styles finishing styles layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,26 +1,35 @@
 import {Link, withRouter} from 'react-router-dom';
 import React from 'react';
 import {isAuth, signout} from '../helpers/Helpers';
+import '../styles/components/_layout.scss'
 
-const Layout = ({children,history}) => {
+//images
+
+import Logo from '../images/inarix-epi-jaune.png'
+
+const Layout = ({children,history,match}) => {
+
 
     const nav = () => (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-        <Link className="navbar-brand" style={{marginRight:'100px', fontFamily:'Meie Script', fontSize:'35px' }} to='/'>Inarix</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+    <nav className="__nav-bar navbar navbar-expand-lg navbar-light  sticky-top" >
+        <Link className="navbar-brand" style={{marginRight:'', fontSize:'25px' }} to='/'>
+            <img alt='logo' width='50px' height='50px' src={Logo}/><span style={{fontWeight:'bold', color:'#EBEBEB',letterSpacing:'1px'}}>inarix</span>
+        </Link>
+            <button className="navbar-toggler" style={{backgroundColor:'#FFC100'}} type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarColor03">
                 <ul className='navbar-nav mr-auto'>
-                    <li className=" nav-item">
-                        <Link to='/' className=" nav-link" > Home</Link>
+                    
+                    <li className="nav-item">
+                        <Link className="nav-link" to='/' style={{color:'#EBEBEB', fontWeight:'bold'}}>á propos</Link>
                     </li>
                     
                     
                     {!isAuth() && (
                         <>
                     <li className="nav-item">
-                        <Link className="nav-link" to='/signin' style={{color:'#78c2ad'}}>Sign In</Link>
+                        <Link className="nav-link" to='/signin' style={{color:'#EBEBEB', fontWeight:'bold'}}>Sign in</Link>
                     </li>
                     
                         </>
@@ -28,7 +37,7 @@ const Layout = ({children,history}) => {
 
                     {isAuth()  && (
                     <li className="nav-item">
-                        <Link className="nav-link" to="/user/dashboard">
+                        <Link className="nav-link" style={{color:'#EBEBEB', fontWeight:'bold'}} to="/user/dashboard">
                         {isAuth().email}
                         </Link>    
                     </li>    
@@ -36,7 +45,7 @@ const Layout = ({children,history}) => {
                     {isAuth() && (
                     <li className="nav-item">
                         <span className="nav-link"
-                                style={{cursor: 'pointer', color: '#000'}}
+                                style={{cursor:'pointer',color:'#E76450', fontWeight:'bold'}}
                                 onClick={()=>{
                                     signout(()=>{
                                         history.push('/')
